b640f8ae56f8f9ef07e75d7fc6bf826b7a8707f9
零壹 <register_2019@126.com>
2022-09-20 21:57:04 +0800

    feat: 滚动条添加右边距

--- packages/components/TimeSelect/src/time-select.vue
+++ packages/components/TimeSelect/src/time-select.vue
@@ -1,22 +1,22 @@
 <template>
-	<div>
-		<DefaultInput v-model="currSelectedTime" @click="openTimeSelectPanel" />
-		<div v-show="isShowTimeSelectPanel" ref="timeSelectRef" class="time-select">
-			<div
-				v-for="(item, index) in initTimeSelectPanel(pickerOptions)"
-				:key="index"
-				:class="['time-select-item', initSelectedTimeStyle(item)]"
-				@click="selectTime(item)"
-			>
-				{{ item }}
-			</div>
-		</div>
-	</div>
+  <div>
+    <DefaultInput v-model="currSelectedTime" @click="openTimeSelectPanel" />
+    <div v-show="isShowTimeSelectPanel" ref="timeSelectRef" class="time-select">
+      <div
+        v-for="(item, index) in initTimeSelectPanel(pickerOptions)"
+        :key="index"
+        :class="['time-select-item', initSelectedTimeStyle(item)]"
+        @click="selectTime(item)"
+      >
+        {{ item }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 export default {
-	name: "DefaultTimeSelect",
+  name: "DefaultTimeSelect",
 };
 </script>
 <script lang="ts" setup>
@@ -26,69 +26,73 @@ import { onClickOutside } from "@vueuse/core";
 import { initTimeSelectPanel, PickerOptions } from "../../../utils/timeSelect";
 
 type Props = {
-	pickerOptions?: PickerOptions;
+  pickerOptions?: PickerOptions;
 };
 
 const { pickerOptions } = withDefaults(defineProps<Props>(), {
-	pickerOptions: (): PickerOptions => ({
-		start: "08:30",
-		step: "00:13",
-		end: "18:30",
-	}),
+  pickerOptions: (): PickerOptions => ({
+    start: "08:30",
+    step: "00:13",
+    end: "18:30",
+  }),
 });
 
 const isShowTimeSelectPanel = ref(false);
 const timeSelectRef = ref();
 
 const openTimeSelectPanel = () => {
-	isShowTimeSelectPanel.value = true;
+  isShowTimeSelectPanel.value = true;
 };
 
 onClickOutside(timeSelectRef, () => {
-	isShowTimeSelectPanel.value = false;
+  isShowTimeSelectPanel.value = false;
 });
 
 const currSelectedTime = ref("");
 const selectTime = (time: string) => {
-	currSelectedTime.value = time;
-	isShowTimeSelectPanel.value = false;
+  currSelectedTime.value = time;
+  isShowTimeSelectPanel.value = false;
 };
 const initSelectedTimeStyle = (val: string) => {
-	if (currSelectedTime.value === val) {
-		return "currSelectedTimeStyle";
-	}
-	return "";
+  if (currSelectedTime.value === val) {
+    return "currSelectedTimeStyle";
+  }
+  return "";
 };
 </script>
 <style lang="scss" scoped>
 .time-select {
-	position: relative;
-	height: 200px;
-	overflow: auto;
-	border: 1px solid #d3d3d3;
-	border-radius: 5px;
-	margin-top: 10px;
-	&-item {
-		padding: 8px 10px;
-		font-size: 14px;
-		line-height: 20px;
-		cursor: pointer;
-	}
-	&-item:hover {
-		background-color: #f5f7fa;
-		font-weight: 700;
-	}
+  position: relative;
+  height: 200px;
+  overflow: auto;
+  border: 1px solid #d3d3d3;
+  border-radius: 5px;
+  margin-top: 10px;
+  z-index: 100;
+  &-item {
+    padding: 8px 10px;
+    font-size: 14px;
+    line-height: 20px;
+    cursor: pointer;
+  }
+  &-item:hover {
+    background-color: #f5f7fa;
+    font-weight: 700;
+  }
 }
 .time-select::-webkit-scrollbar {
-	width: 5px;
+  position: absolute;
+  width: 10px;
+  z-index: 10;
 }
 .time-select::-webkit-scrollbar-thumb {
-	background-color: #eeeeee;
-	border-radius: 10px;
+  background-color: #eeeeee;
+  border: 1px solid #fff;
+  border-radius: 5px;
 }
 
 .currSelectedTimeStyle {
-	color: #409eff;
-	font-weight: 700;
+  color: #409eff;
+  font-weight: 700;
 }
 </style>
