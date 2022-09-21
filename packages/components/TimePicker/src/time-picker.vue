dfedb11a53986892a4b911b581636928e96226a4
gushengquan <register_2019@126.com>
2022-09-21 15:27:19 +0800

    feat: time-picker支持添加默认值

--- packages/components/TimePicker/src/time-picker.vue
+++ packages/components/TimePicker/src/time-picker.vue
@@ -1,42 +1,42 @@
 <template>
-  <div>
-    <DefaultInput
-      class="dc-dialog-input"
-      :size="size"
-      @click="inputFocus"
-      v-model="inputRef"
-    />
+	<div>
+		<DefaultInput
+			class="dc-dialog-input"
+			:size="size"
+			@click="inputFocus"
+			v-model="inputRef"
+		/>
 
-    <div v-if="timePickerStatus" ref="timePickerRef" class="dc-outer">
-      <div class="dc-time-picker">
-        <ul
-          class="dc-ul"
-          v-for="item in showUlNum"
-          :key="item.count"
-          :id="item.id"
-        >
-          <li
-            class="dc-li"
-            v-for="list in item.count"
-            :key="list"
-            :ref="item.liRef"
-            @click="selectTime(item.liRef, timeFormat(list - 1), item.id)"
-          >
-            {{ timeFormat(list - 1) }}
-          </li>
-        </ul>
-      </div>
-      <div class="dc-footer">
-        <a class="dc-footer-cancel" @click="cancelBtn">取消</a>
-        <a class="dc-footer-submit" @click="submitBtn">确定</a>
-      </div>
-    </div>
-  </div>
+		<div v-if="timePickerStatus" ref="timePickerRef" class="dc-outer">
+			<div class="dc-time-picker">
+				<ul
+					class="dc-ul"
+					v-for="item in showUlNum"
+					:key="item.count"
+					:id="item.id"
+				>
+					<li
+						class="dc-li"
+						v-for="list in item.count"
+						:key="list"
+						:ref="item.liRef"
+						@click="selectTime(item.liRef, timeFormat(list - 1), item.id)"
+					>
+						{{ timeFormat(list - 1) }}
+					</li>
+				</ul>
+			</div>
+			<div class="dc-footer">
+				<a class="dc-footer-cancel" @click="cancelBtn">取消</a>
+				<a class="dc-footer-submit" @click="submitBtn">确定</a>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 export default {
-  name: "DefaultTimePicker",
+	name: "DefaultTimePicker",
 };
 </script>
 
@@ -56,54 +56,67 @@ const currMinu = ref("20");
 const currSeco = ref("00");
 
 type Props = {
-  showCategory?: string[];
-  isMountBody?: boolean;
-  modelValue?: string;
-  size?: string;
+	showCategory?: string[];
+	isMountBody?: boolean;
+	modelValue?: string;
+	size?: string;
 };
 
-const props = withDefaults(defineProps<Props>(), {
-  showCategory: () => ["hour", "minu", "seco"],
-  isMountBody: true,
-  modelValue: "",
-});
+const { showCategory, isMountBody, modelValue, size } = withDefaults(
+	defineProps<Props>(),
+	{
+		showCategory: () => ["hour", "minu", "seco"],
+		isMountBody: true,
+		modelValue: "",
+	}
+);
 
 const emit = defineEmits(["update:modelValue"]);
 
+if (modelValue) {
+	inputRef.value = modelValue;
+	const modelValueArr = modelValue.split(":");
+	currHour.value = modelValueArr[0];
+	currMinu.value = modelValueArr[1];
+	currSeco.value = modelValueArr[2];
+}
+
 let showUlNum = computed(() => {
-  return ulList.filter((item) => props.showCategory.includes(item.id));
+	return ulList.filter((item) => showCategory.includes(item.id));
 });
 let inputValue = computed(() => {
-  if (!props.showCategory.includes("seco")) {
-    return currHour.value + ":" + currMinu.value;
-  } else if (!props.showCategory.includes("hour")) {
-    return currMinu.value + ":" + currSeco.value;
-  }
+	if (!showCategory.includes("seco")) {
+		return currHour.value + ":" + currMinu.value;
+	} else if (!showCategory.includes("hour")) {
+		return currMinu.value + ":" + currSeco.value;
+	}
 
-  return currHour.value + ":" + currMinu.value + ":" + currSeco.value;
+	return currHour.value + ":" + currMinu.value + ":" + currSeco.value;
 });
 
 const timeFormat = (num: number) => {
-  return num < 10 ? "0" + num : num.toString();
+	return num < 10 ? "0" + num : num.toString();
 };
 
+let initValOfInputRef = "";
 const inputFocus = () => {
-  timePickerStatus.value = true;
-  const temList = [currHour, currMinu, currSeco];
+	initValOfInputRef = inputRef.value;
+	timePickerStatus.value = true;
+	const temList = [currHour, currMinu, currSeco];
 
-  setTimeout(() => {
-    showUlNum.value.forEach((item, index) => {
-      const ulDom = document.querySelector(`#${item.id}`)!;
+	setTimeout(() => {
+		showUlNum.value.forEach((item, index) => {
+			const ulDom = document.querySelector(`#${item.id}`)!;
 
-      selectedTime(item.liRef, ulDom, temList[index], item.id);
-    });
-  }, 200);
+			selectedTime(item.liRef, ulDom, temList[index], item.id);
+		});
+	}, 200);
 };
 onClickOutside(timePickerRef, () => {
-  timePickerStatus.value = false;
+	timePickerStatus.value = false;
 });
 watch(inputValue, (val) => {
-  inputRef.value = val;
+	inputRef.value = val;
 });
 /**
  * 点击时分秒 滚动到指定位置
@@ -112,113 +125,115 @@ watch(inputValue, (val) => {
  * @param id
  */
 const selectTime = (el: any, val: string, id: string) => {
-  let top = el.value[Number(val)].offsetTop - 65;
+	let top = el.value[Number(val)].offsetTop - 65;
 
-  const ulDom = document.querySelector(`#${id}`)!;
-  ulDom.scrollTo({
-    top,
-  });
+	const ulDom = document.querySelector(`#${id}`)!;
+	ulDom.scrollTo({
+		top,
+	});
 };
 
 const cancelBtn = () => {
-  emit("update:modelValue", inputRef.value);
-  timePickerStatus.value = false;
+	inputRef.value = initValOfInputRef;
+	emit("update:modelValue", inputRef.value);
+	timePickerStatus.value = false;
 };
 const submitBtn = () => {
-  emit("update:modelValue", inputRef.value);
-  timePickerStatus.value = false;
+	emit("update:modelValue", inputRef.value);
+	timePickerStatus.value = false;
+	initValOfInputRef = inputRef.value;
 };
 
 onMounted(() => {
-  emit("update:modelValue", inputRef.value);
+	emit("update:modelValue", inputRef.value);
 });
 </script>
 
 <style lang="scss" scoped>
 .dc-dialog-input {
-  height: 32px;
-  line-height: 32px;
+	height: 32px;
+	line-height: 32px;
 }
 .dc-outer {
-  position: absolute;
-  background-color: #fff;
-  border: 1px solid #e4e7ed;
+	position: absolute;
+	background-color: #fff;
+	border: 1px solid #e4e7ed;
 }
 .dc-time-picker {
-  display: flex;
-  height: 170px;
-  width: 160px;
-  overflow: hidden;
-  border-bottom: 1px solid #e4e7ed;
+	display: flex;
+	height: 170px;
+	width: 160px;
+	overflow: hidden;
+	border-bottom: 1px solid #e4e7ed;
 }
 .dc-footer {
-  display: flex;
-  justify-content: right;
-  margin: 10px 0;
-  &-cancel,
-  &-submit {
-    background-color: #fff;
-    font-size: 12px;
-    margin: 0 5px;
-    cursor: pointer;
-    letter-spacing: 5px;
-  }
-  &-submit {
-    color: #409eff;
-  }
+	display: flex;
+	justify-content: right;
+	margin: 10px 0;
+	&-cancel,
+	&-submit {
+		background-color: #fff;
+		font-size: 12px;
+		margin: 0 5px;
+		cursor: pointer;
+		letter-spacing: 5px;
+	}
+	&-submit {
+		color: #409eff;
+	}
 }
 .dc-time-picker::before,
 .dc-time-picker::after {
-  content: "";
-  top: 40%;
-  position: absolute;
-  margin: -16px 12% 0 12%;
-  height: 32px;
-  z-index: 100;
-  left: 0;
-  right: 0;
-  box-sizing: border-box;
-  padding-top: 6px;
-  text-align: left;
-  border-top: 1px solid #e4e7ed;
-  border-bottom: 1px solid #e4e7ed;
+	content: "";
+	top: 40%;
+	position: absolute;
+	margin: -16px 12% 0 12%;
+	height: 32px;
+	z-index: 100;
+	left: 0;
+	right: 0;
+	box-sizing: border-box;
+	padding-top: 6px;
+	text-align: left;
+	border-top: 1px solid #e4e7ed;
+	border-bottom: 1px solid #e4e7ed;
 }
 .dc-ul {
-  overflow: hidden;
-  scroll-snap-type: y mandatory;
-  flex: 1;
-  padding: 0;
-  margin: 0;
-  z-index: 101;
-  text-align: center;
+	overflow: hidden;
+	scroll-snap-type: y mandatory;
+	flex: 1;
+	padding: 0;
+	margin: 0;
+	z-index: 101;
+	text-align: center;
 }
 .dc-ul::-webkit-scrollbar {
-  width: 5px;
-  background-color: none;
+	width: 5px;
+	background-color: none;
 }
 .dc-ul::-webkit-scrollbar-thumb {
-  background-color: #eeeeee;
-  border-radius: 10px;
+	background-color: #eeeeee;
+	border-radius: 10px;
 }
 .dc-ul::before,
 .dc-ul::after {
-  content: "";
-  display: block;
-  width: 100%;
-  height: 68px;
+	content: "";
+	display: block;
+	width: 100%;
+	height: 68px;
 }
 .dc-ul:hover {
-  overflow-y: auto;
+	overflow-y: auto;
 }
 .dc-li {
-  list-style: none;
-  text-align: center;
-  width: 40px;
-  height: 32px;
-  line-height: 32px;
-  font-size: 12px;
-  scroll-snap-align: center;
-  cursor: pointer;
-  user-select: none;
+	list-style: none;
+	text-align: center;
+	width: 40px;
+	height: 32px;
+	line-height: 32px;
+	font-size: 12px;
+	scroll-snap-align: center;
+	cursor: pointer;
+	user-select: none;
 }
 </style>
