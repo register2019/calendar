cff8c5d8afd02d4fbc7fcca767b9a012e0cbc56d
gushengquan <register_2019@126.com>
2022-10-14 17:21:44 +0800

    fix: 修复存在默认值时 修改面板输入框动态改变

--- packages/components/Calendar/src/panelInput.vue
+++ packages/components/Calendar/src/panelInput.vue
@@ -1,27 +1,27 @@
 <template>
-  <span class="dc-calendar-header-input">
-    <span>
-      <DefaultInput
-        size="small"
-        v-model="inputDate"
-        :disabled="inputIsDisabled"
-      />
-    </span>
-    <span>
-      <DefaultTimeSelect
-        v-if="timeType === 'Select'"
-        size="small"
-        v-model="inputTime"
-        :disabled="inputIsDisabled"
-      />
-      <DefaultTimePicker
-        v-else
-        size="small"
-        v-model="inputTime"
-        :disabled="inputIsDisabled"
-      />
-    </span>
-  </span>
+	<span class="dc-calendar-header-input">
+		<span>
+			<DefaultInput
+				size="small"
+				v-model="inputDate"
+				:disabled="inputIsDisabled"
+			/>
+		</span>
+		<span>
+			<DefaultTimeSelect
+				v-if="timeType === 'Select'"
+				size="small"
+				v-model="inputTime"
+				:disabled="inputIsDisabled"
+			/>
+			<DefaultTimePicker
+				v-else
+				size="small"
+				v-model="inputTime"
+				:disabled="inputIsDisabled"
+			/>
+		</span>
+	</span>
 </template>
 
 <script lang="ts" setup>
@@ -31,16 +31,16 @@ import DefaultTimeSelect from "../../TimeSelect/src/time-select.vue";
 import DefaultTimePicker from "../../TimePicker/src/time-picker.vue";
 
 type Props = {
-  timeType?: string;
-  inputIsDisabled?: boolean;
-  date: string;
-  time?: string;
+	timeType?: string;
+	inputIsDisabled?: boolean;
+	date: string;
+	time?: string;
 };
 
 const props = withDefaults(defineProps<Props>(), {
-  timeType: "Picker",
-  inputIsDisabled: false,
-  date: "",
+	timeType: "Picker",
+	inputIsDisabled: false,
+	date: "",
 });
 const emit = defineEmits(["update:date", "update:time"]);
 
@@ -48,30 +48,31 @@ const inputDate = ref("");
 const inputTime = ref("");
 
 watch(inputDate, (val) => {
-  emit("update:date", val);
+	emit("update:date", val);
 });
 watch(inputTime, (val) => {
-  emit("update:time", val);
+	emit("update:time", val);
 });
 
 watch(
-  () => props,
-  (val) => {
-    inputDate.value = val.date;
-    inputTime.value = val.time!;
-  },
-  {
-    deep: true,
-  }
+	() => props,
+	(val) => {
+		inputDate.value = val.date;
+		inputTime.value = val.time!;
+	},
+	{
+		deep: true,
+		immediate: true,
+	}
 );
 </script>
 
 <style lang="scss" scoped>
 .dc-calendar-header-input {
-  display: table-cell;
-  span {
-    display: table-cell;
-    padding: 0 5px;
-  }
+	display: table-cell;
+	span {
+		display: table-cell;
+		padding: 0 5px;
+	}
 }
 </style>
