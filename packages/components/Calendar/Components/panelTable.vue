7c50401f5a1f28f24e00d734b72c2f84a0934c60
gushengquan <register_2019@126.com>
2022-10-26 13:42:18 +0800

    feat: panelTable面板动态更新问题

--- packages/components/Calendar/Components/panelTable.vue
+++ packages/components/Calendar/Components/panelTable.vue
@@ -1,127 +1,134 @@
 <template>
-  <table class="dc-table">
-    <thead>
-      <tr>
-        <th v-for="item in tableHeader" :key="item">{{ item }}</th>
-      </tr>
-    </thead>
-    <tbody v-for="(list, index) in tableTds" :key="index">
-      <td
-        v-for="(td, index) in list"
-        :key="index"
-        :class="[tdStyle, selectedDateBgUI(td)]"
-        @click="selectedDate(td)"
-      >
-        <div @mouseenter="dynamicSelection(td)">
-          <span
-            :class="[
-              todayUI(td.timestamp),
-              beforeAndAfterUI(td.category),
-              selectedStartAndEndUI(td),
-              dateTimeTypeUI(td),
-            ]"
-            >{{ td.value }}</span
-          >
-        </div>
-      </td>
-    </tbody>
-  </table>
+	<table class="dc-table">
+		<thead>
+			<tr>
+				<th v-for="item in tableHeader" :key="item">{{ item }}</th>
+			</tr>
+		</thead>
+		<tbody v-for="(list, index) in tableTds" :key="index">
+			<td
+				v-for="(td, index) in list"
+				:key="index"
+				:class="[tdStyle, selectedDateBgUI(td)]"
+				@click="selectedDate(td)"
+			>
+				<div @mouseenter="dynamicSelection(td)">
+					<span
+						:class="[
+							todayUI(td.timestamp),
+							beforeAndAfterUI(td.category),
+							selectedStartAndEndUI(td),
+							dateTimeTypeUI(td),
+						]"
+						>{{ td.value }}</span
+					>
+				</div>
+			</td>
+		</tbody>
+	</table>
 </template>
 
 <script lang="ts" setup>
 import { ref, watch } from "vue";
 import {
-  getTodayTimeStamp,
-  IDate,
-  primaryColor,
-  tableHeader,
-  secondaryColor,
-  bgColor,
-  whiteColor,
-  getTimeUtils,
-  dateToTimeStamp,
+	getTodayTimeStamp,
+	IDate,
+	primaryColor,
+	tableHeader,
+	secondaryColor,
+	bgColor,
+	whiteColor,
+	getTimeUtils,
+	dateToTimeStamp,
 } from "../../../utils";
 
 type Props = {
-  tds: IDate[];
-  selectedDateList?: number[];
-  isSelectedFinish?: boolean;
-  type?: string;
-  currDateTime?: string; // 当且仅当类型为DateTime是使用
+	tds: IDate[];
+	selectedDateList?: number[];
+	isSelectedFinish?: boolean;
+	type?: string;
+	currDateTime?: string; // 当且仅当类型为DateTime是使用
 };
 
 const props = withDefaults(defineProps<Props>(), {
-  selectedDateList: (): number[] => [],
-  type: "DateTimePicker",
+	selectedDateList: (): number[] => [],
+	type: "DateTimePicker",
 });
 const emit = defineEmits(["emitSelectedDate"]);
 const tdStyle = "dc-table-tbody-td";
 
 const tableTds = ref<IDate[][]>([]);
 const initTableData = (tds: IDate[]) => {
-  for (let i = 0; i < 6; i++) {
-    tableTds.value[i] = [];
-  }
-  let i = 0;
+	for (let i = 0; i < 6; i++) {
+		tableTds.value[i] = [];
+	}
+	let i = 0;
 
-  tds.forEach((td, index) => {
-    if ((index + 1) % 7 === 0) {
-      tableTds.value[i].push(td);
-      i++;
-    } else {
-      tableTds.value[i].push(td);
-    }
-  });
-
-  console.log(tableTds.value);
+	tds.forEach((td, index) => {
+		if ((index + 1) % 7 === 0) {
+			tableTds.value[i].push(td);
+			i++;
+		} else {
+			tableTds.value[i].push(td);
+		}
+	});
 };
-initTableData(props.tds);
+
+watch(
+	() => props.tds,
+	(val) => {
+		initTableData(val);
+	},
+	{
+		immediate: true,
+	}
+);
 
 /**
  * 日历面板中与今天对应日期的UI
  * @param val
  */
 const todayUI = (val: number) => {
-  if (val === getTodayTimeStamp()) {
-    return "dc-table-today";
-  }
-  return "";
+	if (val === getTodayTimeStamp()) {
+		return "dc-table-today";
+	}
+	return "";
 };
 /**
  * 日历面板中上月和下月的UI
  * @param category
  */
 const beforeAndAfterUI = (category: string) => {
-  if (category !== "curr") {
-    return "dc-table-isNotCurMonth";
-  }
-  return "";
+	if (category !== "curr") {
+		return "dc-table-isNotCurMonth";
+	}
+	return "";
 };
 
 const initPropsSelectedDateList = () => {
-  const {
-    year: startYear,
-    month: startMonth,
-    day: startDay,
-  } = getTimeUtils(props.selectedDateList[0]);
-  const {
-    year: endYear,
-    month: endMonth,
-    day: endDay,
-  } = getTimeUtils(
-    props.selectedDateList[1] === undefined
-      ? props.selectedDateList[0]
-      : props.selectedDateList[1]
-  );
+	const {
+		year: startYear,
+		month: startMonth,
+		day: startDay,
+	} = getTimeUtils(props.selectedDateList[0]);
+	const {
+		year: endYear,
+		month: endMonth,
+		day: endDay,
+	} = getTimeUtils(
+		props.selectedDateList[1] === undefined
+			? props.selectedDateList[0]
+			: props.selectedDateList[1]
+	);
 
-  const startDateTime = dateToTimeStamp(
-    startYear + "-" + startMonth + "-" + startDay + " " + "00:00:00"
-  );
-  const endDateTime = dateToTimeStamp(
-    endYear + "-" + endMonth + "-" + endDay + " " + "00:00:00"
-  );
+	const startDateTime = dateToTimeStamp(
+		startYear + "-" + startMonth + "-" + startDay + " " + "00:00:00"
+	);
+	const endDateTime = dateToTimeStamp(
+		endYear + "-" + endMonth + "-" + endDay + " " + "00:00:00"
+	);
 
-  return [startDateTime, endDateTime];
+	return [startDateTime, endDateTime];
 };
 
 /**
@@ -129,45 +136,45 @@ const initPropsSelectedDateList = () => {
  * @param td
  */
 const selectedDateBgUI = (td: IDate) => {
-  if (props.selectedDateList.length === 0) return;
-  if (td.category === "curr") {
-    const sortSelectedDateList = initPropsSelectedDateList();
-    sortSelectedDateList.sort((a, b) => a - b);
-    if (
-      (props.selectedDateList.length === 2 &&
-        sortSelectedDateList[0] === td.timestamp &&
-        sortSelectedDateList[1] === td.timestamp) ||
-      (props.selectedDateList.length === 1 &&
-        sortSelectedDateList[0] === td.timestamp)
-    ) {
-      return "dc-table-selected-td start end";
-    }
+	if (props.selectedDateList.length === 0) return;
+	if (td.category === "curr") {
+		const sortSelectedDateList = initPropsSelectedDateList();
+		sortSelectedDateList.sort((a, b) => a - b);
+		if (
+			(props.selectedDateList.length === 2 &&
+				sortSelectedDateList[0] === td.timestamp &&
+				sortSelectedDateList[1] === td.timestamp) ||
+			(props.selectedDateList.length === 1 &&
+				sortSelectedDateList[0] === td.timestamp)
+		) {
+			return "dc-table-selected-td start end";
+		}
 
-    if (sortSelectedDateList[0] === td.timestamp) {
-      return "dc-table-selected-td start";
-    }
-    if (sortSelectedDateList[1] === td.timestamp) {
-      return "dc-table-selected-td end";
-    }
+		if (sortSelectedDateList[0] === td.timestamp) {
+			return "dc-table-selected-td start";
+		}
+		if (sortSelectedDateList[1] === td.timestamp) {
+			return "dc-table-selected-td end";
+		}
 
-    if (
-      sortSelectedDateList[0] < td.timestamp &&
-      sortSelectedDateList[1] > td.timestamp
-    ) {
-      return "dc-table-selected-td";
-    }
-  }
-  return "";
+		if (
+			sortSelectedDateList[0] < td.timestamp &&
+			sortSelectedDateList[1] > td.timestamp
+		) {
+			return "dc-table-selected-td";
+		}
+	}
+	return "";
 };
 const selectedStartAndEndUI = (td: IDate) => {
-  if (props.selectedDateList.length === 0) return;
-  if (
-    td.category === "curr" &&
-    (initPropsSelectedDateList()[0] === td.timestamp ||
-      initPropsSelectedDateList()[1] === td.timestamp)
-  ) {
-    return "dc-table-selected-td-boundary";
-  }
+	if (props.selectedDateList.length === 0) return;
+	if (
+		td.category === "curr" &&
+		(initPropsSelectedDateList()[0] === td.timestamp ||
+			initPropsSelectedDateList()[1] === td.timestamp)
+	) {
+		return "dc-table-selected-td-boundary";
+	}
 };
 
 const selectFinish = ref(false);
@@ -175,23 +182,23 @@ const selectFinish = ref(false);
  * 解决在一个面板中完成了日期选择 另一个面板没有选中 当移入另一个时出现选中的情况
  */
 watch(
-  () => props.isSelectedFinish,
-  (val) => {
-    if (val) {
-      selectFinish.value = val;
-    }
-  },
-  {
-    immediate: true,
-  }
+	() => props.isSelectedFinish,
+	(val) => {
+		if (val) {
+			selectFinish.value = val;
+		}
+	},
+	{
+		immediate: true,
+	}
 );
 const dynamicSelection = (td: IDate) => {
-  if (props.selectedDateList.length === 1) {
-    selectFinish.value = false;
-  }
-  if (props.selectedDateList.length >= 1 && !selectFinish.value) {
-    emit("emitSelectedDate", td, "mouse");
-  }
+	if (props.selectedDateList.length === 1) {
+		selectFinish.value = false;
+	}
+	if (props.selectedDateList.length >= 1 && !selectFinish.value) {
+		emit("emitSelectedDate", td, "mouse");
+	}
 };
 
 /**
@@ -199,80 +206,80 @@ const dynamicSelection = (td: IDate) => {
  */
 const currSelectedDateTime = ref<number>();
 const dateTimeTypeUI = (td: IDate) => {
-  if (
-    td.category === "curr" &&
-    td.timestamp === currSelectedDateTime.value &&
-    props.type === "DateTime"
-  ) {
-    return "dc-table-selected-date-time";
-  }
+	if (
+		td.category === "curr" &&
+		td.timestamp === currSelectedDateTime.value &&
+		props.type === "DateTime"
+	) {
+		return "dc-table-selected-date-time";
+	}
 };
 
 watch(
-  () => props.currDateTime,
-  (val) => {
-    currSelectedDateTime.value = dateToTimeStamp(val + " " + "00:00:00");
-  }
+	() => props.currDateTime,
+	(val) => {
+		currSelectedDateTime.value = dateToTimeStamp(val + " " + "00:00:00");
+	}
 );
 
 const selectedDate = (td: IDate) => {
-  if (props.selectedDateList.length === 2) {
-    selectFinish.value = true;
-  }
-  currSelectedDateTime.value = td.timestamp;
-  emit("emitSelectedDate", td, "click");
+	if (props.selectedDateList.length === 2) {
+		selectFinish.value = true;
+	}
+	currSelectedDateTime.value = td.timestamp;
+	emit("emitSelectedDate", td, "click");
 };
 </script>
 
 <style lang="scss" scoped>
 .dc-table {
-  min-width: 291px;
-  border-spacing: 0 10px !important;
-  &-tbody-td {
-    text-align: center;
-    cursor: pointer;
-    height: 38px;
-    width: 38px;
-    font-size: 12px;
-  }
-  &-tbody-td:hover {
-    color: v-bind(primaryColor);
-  }
-  &-today {
-    color: v-bind(primaryColor);
-    font-weight: 700;
-  }
-  &-isNotCurMonth {
-    color: v-bind(secondaryColor);
-  }
-  &-selected-td {
-    background-color: v-bind(bgColor);
-    &-boundary {
-      display: inline-block;
-      width: 24px;
-      height: 24px;
-      line-height: 24px;
-      background-color: v-bind(primaryColor);
-      border-radius: 50%;
-      color: v-bind(whiteColor);
-    }
-  }
-  &-selected-date-time {
-    width: 24px;
-    height: 24px;
-    line-height: 24px;
-    border-radius: 50%;
-    display: inline-block;
-    background-color: v-bind(primaryColor);
-    color: #fff;
-  }
+	min-width: 291px;
+	border-spacing: 0 10px !important;
+	&-tbody-td {
+		text-align: center;
+		cursor: pointer;
+		height: 38px;
+		width: 38px;
+		font-size: 12px;
+	}
+	&-tbody-td:hover {
+		color: v-bind(primaryColor);
+	}
+	&-today {
+		color: v-bind(primaryColor);
+		font-weight: 700;
+	}
+	&-isNotCurMonth {
+		color: v-bind(secondaryColor);
+	}
+	&-selected-td {
+		background-color: v-bind(bgColor);
+		&-boundary {
+			display: inline-block;
+			width: 24px;
+			height: 24px;
+			line-height: 24px;
+			background-color: v-bind(primaryColor);
+			border-radius: 50%;
+			color: v-bind(whiteColor);
+		}
+	}
+	&-selected-date-time {
+		width: 24px;
+		height: 24px;
+		line-height: 24px;
+		border-radius: 50%;
+		display: inline-block;
+		background-color: v-bind(primaryColor);
+		color: #fff;
+	}
 }
 .start {
-  border-top-left-radius: 50%;
-  border-bottom-left-radius: 50%;
+	border-top-left-radius: 50%;
+	border-bottom-left-radius: 50%;
 }
 .end {
-  border-top-right-radius: 50%;
-  border-bottom-right-radius: 50%;
+	border-top-right-radius: 50%;
+	border-bottom-right-radius: 50%;
 }
 </style>
