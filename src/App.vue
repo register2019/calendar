4e54342c4b32d7373151639c02af237bbf465247
gushengquan <register_2019@126.com>
2022-12-26 14:42:34 +0800

    feat: DateTimePicker类型设置暗色主图

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
 	<div>
 		<DefaultCalendar
-			type="DatePicker"
+			v-model="selectedTimeRange"
+			@onClick="getSelectedTimeRange"
+			:pickerOptions="pickerOptions"
+			timeType="Select"
+			format="yyyy-MM-DD HH"
+			pickerFormat="HH:mm"
+			:i18n="i18nGlobal"
 			:theme="themeGlobal"
-			:picker-options="pickerOptions"
+			type="DateTimePicker"
 			:selectOptions="selectOptions"
 			:disabledDate="disabledDate"
-			@onClick="getSelectedTime"
 		/>
 		<DefaultButton @click="changeLang">change</DefaultButton>
 	</div>
@@ -14,42 +19,44 @@
 
 <script lang="ts" setup>
 import { ref, reactive, watch, computed } from "vue";
-import i18n from "./locale";
-const pickerOptions = [
-	{
-		text: "今天",
-		value: () => new Date().getTime(),
-	},
-	{
-		text: "昨天",
-		value: () => new Date().getTime() - 3600 * 1000 * 24,
-	},
-	{
-		text: "一周前",
-		value: () => new Date().getTime() - 3600 * 1000 * 24 * 7,
-	},
-];
-const selectOptions = {
-	start: "08:00",
-	step: "00:15",
-	end: "18:30",
-};
-const getSelectedTime = (val: number) => {
-	console.log(val);
-};
-const disabledDate = {
-	type: "range",
-	range: ["2022-11-06 00:00:00", "2022-11-16 00:00:00"],
-};
+import i18n, { t } from "./locale";
 
-const themeGlobal = ref("dark");
 const i18nGlobal = ref("zh");
+const themeGlobal = ref("dark");
 const changeLang = () => {
 	themeGlobal.value = themeGlobal.value === "dark" ? "light" : "dark";
-	console.log("------->", themeGlobal.value);
 
 	// i18nGlobal.value = i18nGlobal.value === "zh" ? "en" : "zh";
 	// i18n.global.locale = i18nGlobal.value === "zh" ? "zh" : "en";
+};
+
+const selectedTimeRange = ref<[Date, Date]>([
+	new Date(2000, 10, 10, 10, 14),
+	new Date(2000, 10, 11, 12, 24),
+]);
+// const selectedTimeRange = ref<Date[]>([]);
+const pickerOptions = [
+	{
+		text: "最近一周",
+		value: () => {
+			const end = new Date().getTime();
+			const start = new Date().getTime() - 3600 * 1000 * 24 * 7;
+
+			return [start, end];
+		},
+	},
+];
+const selectOptions = {
+	start: "08:30",
+	step: "00:15",
+	end: "18:30",
+};
+const getSelectedTimeRange = (val: number[]) => {
+	console.log(val);
+};
+const disabledDate = {
+	type: "today",
+	range: "2022-11-06 00:00:00",
 };
 </script>
 
